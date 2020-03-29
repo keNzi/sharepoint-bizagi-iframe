0d69a2dfe7a53c99388dd7581aaacb4c09984067
keNzi <piotr.czajkowski@archicom.pl>
2020-03-29 15:15:49 +0200

    Initial commit.

---
+++ src/webparts/bizagiBpmn/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBizagiBpmnWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BizagiBpmnWebPartStrings' {
+  const strings: IBizagiBpmnWebPartStrings;
+  export = strings;
+}
